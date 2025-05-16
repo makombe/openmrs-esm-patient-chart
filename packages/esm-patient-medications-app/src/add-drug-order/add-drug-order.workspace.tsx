1ed717f3fb2f67ca2e29bdc307728b501538e29a
makombe <kennedymakombe@gmail.com>
2025-05-16 15:49:07 +0300

    Fix order datetime to be after encounter datetime

--- packages/esm-patient-medications-app/src/add-drug-order/add-drug-order.workspace.tsx
+++ packages/esm-patient-medications-app/src/add-drug-order/add-drug-order.workspace.tsx
@@ -33,6 +33,8 @@ export default function AddDrugOrderWorkspace({
   const { orders, setOrders } = useOrderBasket<DrugOrderBasketItem>('medications', prepMedicationOrderPostData);
   const [currentOrder, setCurrentOrder] = useState(initialOrder);
   const session = useSession();
+  const { currentVisit } = useVisitOrOfflineVisit(patientUuid);
+  const visitStartDate = currentVisit?.startDatetime;
 
   const cancelDrugOrder = useCallback(() => {
     closeWorkspace({
@@ -57,6 +59,14 @@ export default function AddDrugOrderWorkspace({
     (finalizedOrder: DrugOrderBasketItem) => {
       finalizedOrder.careSetting = careSettingUuid;
       finalizedOrder.orderer = session.currentProvider.uuid;
+      if (visitStartDate && finalizedOrder.dateActivated) {
+        const visitStart = new Date(visitStartDate).getTime();
+        const selectedDate = new Date(finalizedOrder.dateActivated).getTime();
+        if (selectedDate <= visitStart) {
+          // If same date or earlier time, push it slightly ahead (e.g., +1 second)
+          finalizedOrder.dateActivated = new Date(visitStart + 3000); // 3 second later
+        }
+      }
 
       const newOrders = [...orders];
       const existingOrder = orders.find((order) => ordersEqual(order, finalizedOrder));
@@ -74,7 +84,7 @@ export default function AddDrugOrderWorkspace({
         onWorkspaceClose: () => launchPatientWorkspace('order-basket'),
       });
     },
-    [orders, setOrders, closeWorkspaceWithSavedChanges, session.currentProvider.uuid],
+    [orders, setOrders, closeWorkspaceWithSavedChanges, session.currentProvider.uuid, visitStartDate],
   );
 
   if (!currentOrder) {
